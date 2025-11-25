0f3fd02b31751850565e3ebe11c272a2f4d75ecd
Sololiquy <alfathaditya11@gmail.com>
2025-11-25 17:28:33 +0700

    UI update

--- src/components/card/projectCard.tsx
+++ src/components/card/projectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import LinkIcon from "assets/hyperlink.tsx";
 
 type parameter = {
    data: {
@@ -10,9 +10,18 @@ type parameter = {
 export default function ExperienceCard({ data }: parameter) {
    return (
       <>
-         <div className={`w-full p-2 gap-1 flex flex-col bg-(--card-experience-bg)`}>
-            <div>{data.title}</div>
-            <div>{data.url}</div>
+         <div className={`w-full p-2 gap-1 flex flex-row bg-(--card-experience-bg)`}>
+            <div className={`flex flex-col grow`}>
+               <div className={`text-xl font-bold`}>{data.title}</div>
+               {/* <div>{data.url}</div> */}
+            </div>
+            {data.url && (
+               <a href={data.url} target="_blank" rel="noopener noreferrer">
+                  <div className={`size-10 p-2 shrink-0 border rounded-xl`}>
+                     <LinkIcon />
+                  </div>
+               </a>
+            )}
          </div>
       </>
    );
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Outlet, Link } from "react-router";
 import { useT } from "@/lang/_locale";
 
@@ -6,19 +7,24 @@ import LanguageSwitcher from "@/components/button/languageSwitch";
 
 export default function Layout() {
    const text = useT();
+   const [tabIndex, setTabIndex] = useState<number>(0);
    return (
       <div className={`allScreen flex allCenter`}>
-         <div className={`w-300 h-200 gap-1 flex flex-col`}>
-            <div className={`flex flex-col gap-2 absolute top-3 right-3`}>
-               <ThemeSwitch />
-               <LanguageSwitcher />
-            </div>
-            <div
-               className={`h-15 shrink-0 flex flex-row gap-10 text-2xl allCenter p-2 bg-(--content-bg) dark:shadow-[0_0_0_rgba(255,255,255,0.6)] transition`}
-            >
-               <Link to="contact">{text.contact}</Link>
-               <Link to="/experience">{text.experience}</Link>
-               <Link to="/project">{text.project}</Link>
+         <div className={`flex flex-col gap-2 absolute top-3 right-3`}>
+            <ThemeSwitch />
+            <LanguageSwitcher />
+         </div>
+         <div className={`w-300 h-200 flex flex-col`}>
+            <div className={`h-15 shrink-0 flex flex-row text-2xl allCenter`}>
+               <Link className={`tabLink ${tabIndex === 0 ? "tabLink_active" : ""}`} onClick={() => setTabIndex(0)} to="contact">
+                  {text.contact}
+               </Link>
+               <Link className={`tabLink ${tabIndex === 1 ? "tabLink_active" : ""}`} onClick={() => setTabIndex(1)} to="/experience">
+                  {text.experience}
+               </Link>
+               <Link className={`tabLink ${tabIndex === 2 ? "tabLink_active" : ""}`} onClick={() => setTabIndex(2)} to="/project">
+                  {text.project}
+               </Link>
             </div>
             <div className={`h-185 p-2 bg-(--content-bg) dark:shadow-[0_0_5px_rgba(255,255,255,0.6)] transition`}>
                <Outlet />
